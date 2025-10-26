99e8b7927a86afddb6bc99b9196209003fc76c16
Henrik Sivertsgård <henriksive@gmail.com>
2025-10-26 14:05:47 +0100

    add nav

--- src/features/root/Root.tsx
+++ src/features/root/Root.tsx
@@ -1,21 +1,52 @@
 import { Outlet } from "react-router-dom";
 import "./root.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import bilde1 from "../../assets/1.jpg";
+import bilde2 from "../../assets/2.jpg";
+import bilde3 from "../../assets/3.jpg";
+import bilde4 from "../../assets/4.jpg";
+
 export function Root() {
+  const location = useLocation();
+
+  const actualLocation = location.pathname.substring(1).toLowerCase();
+
+  const links = [
+    { to: "/", label: "Hjem" },
+    { to: "bryllupshelgen", label: "Bryllupshelgen" },
+    { to: "lokasjon", label: "Lokasjon" },
+    { to: "overnatting", label: "Overnatting" },
+    { to: "praktisk", label: "Praktisk informasjon" },
+  ];
+
   return (
     <>
       <div className="header">
-        <div>Blomst</div>
-        <div>
-          <Link to="/">Hjem</Link>
-          <Link to="bryllupshelgen">Bryllupshelgen</Link>
-          <Link to="lokasjon">Lokasjon</Link>
-          <Link to="overnatting">Overnatting</Link>
-          <Link to="praktisk">Praktisk informasjon</Link>
+        <div className="blomst">Blomst</div>
+        <div className="linkContainer">
+          {links.map((link) => {
+            const isActive =
+              (link.to === "/" && actualLocation === "") ||
+              actualLocation === link.to.toLowerCase();
+            return (
+              <Link
+                key={link.to}
+                to={link.to}
+                className={isActive ? "active" : ""}
+              >
+                {link.label}
+              </Link>
+            );
+          })}
         </div>
-        <div>Blomst</div>
+        <div className="blomst">Blomst</div>
       </div>
-      <div className="bilderad">BILDERAD</div>
+      <div className="bilderad">
+        <img src={bilde1} alt="Bryllupsbilde" />
+        <img src={bilde2} alt="Bryllupsbilde" className="top-center" />
+        <img src={bilde3} alt="Bryllupsbilde" />
+        <img src={bilde4} alt="Bryllupsbilde" />
+      </div>
       <div className="ramme">
         <Outlet />
       </div>
