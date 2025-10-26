29ac038ce3eff66ad95d7c77e1208e560e78bc2c
Henrik Sivertsgård <henriksive@gmail.com>
2025-10-26 12:45:42 +0100

    fix nav

--- src/features/root/Root.tsx
+++ src/features/root/Root.tsx
@@ -1,9 +1,20 @@
 import { Outlet } from "react-router-dom";
 import "./root.css";
+import { Link } from "react-router-dom";
 export function Root() {
   return (
     <>
-      <div className="header">~LAG EN NAVIGERINGSMENY HER~</div>
+      <div className="header">
+        <div>Blomst</div>
+        <div>
+          <Link to="/">Hjem</Link>
+          <Link to="bryllupshelgen">Bryllupshelgen</Link>
+          <Link to="lokasjon">Lokasjon</Link>
+          <Link to="overnatting">Overnatting</Link>
+          <Link to="praktisk">Praktisk informasjon</Link>
+        </div>
+        <div>Blomst</div>
+      </div>
       <div className="bilderad">BILDERAD</div>
       <div className="ramme">
         <Outlet />
