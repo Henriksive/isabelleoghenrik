44f351563bf42ffe86016b35915653d3e0cb4002
Henrik Sivertsgård <henriksive@gmail.com>
2026-05-10 21:33:02 +0200

    feat: henge etterfest deploy

--- src/features/bryllupshelgen/Bryllupshelgen.tsx
+++ src/features/bryllupshelgen/Bryllupshelgen.tsx
@@ -7,28 +7,31 @@ export const Bryllupshelgen: React.FC = () => {
     <div>
       <h1>Velkommen til Romsdalen</h1>
       <p>Vi gleder oss til å dele helgen med dere!</p>
-      <p><i>Helgens sentrum er Dalsbygdveien 88, 6320 Isfjorden.</i></p>
+      <p>
+        <i>Helgens sentrum er Dalsbygdveien 88, 6320 Isfjorden.</i>
+      </p>
       <h2>Program</h2>
       <div className="subFrame">
         <h3>Fredag 3. juli</h3>
         <p>
-          Vi starter feiringen fredag og håper så mange som mulig kommer på en bli-kjent-kveld på tunet på Grøtta fra klokken 17:00!
-          Vi serverer enkel varmmat og det blir etter hvert låvequiz med snacks
-          og noe godt i glasset. Låven er uisolert, kle deg etter været.
+          Vi starter feiringen fredag og håper så mange som mulig kommer på en
+          bli-kjent-kveld på tunet på Grøtta fra klokken 17:00! Vi serverer
+          enkel varmmat og det blir etter hvert låvequiz med snacks og noe godt
+          i glasset. Låven er uisolert, kle deg etter været.
         </p>
         <h3>Lørdag 4. juli</h3>
         <p>
-          Vielsen finner sted i Hen kirke klokken 13:00. Etter vielsen blir det servert
-          forfriskninger på tunet på Grøtta mens brudeparet fotograferes og man venter på deres
-          ankomst. Middagen serveres fra ca. 17:00. Etter maten åpner dansegulvet
-          og baren på låven.
+          Vielsen finner sted i Hen kirke klokken 13:00. Etter vielsen blir det
+          servert forfriskninger på tunet på Grøtta mens brudeparet fotograferes
+          og man venter på deres ankomst. Middagen serveres fra ca. 17:00. Etter
+          maten åpner dansegulvet og baren på låven.
         </p>
         <h3>Søndag 5. juli</h3>
         <p>
           Alle som vil stikke innom dagen derpå er hjertelig velkomne til å
-          komme til tunet for å spise rester, spille hagespill, ta badstu på
-          låven eller bare henge og debriefe festen. De nygifte vil være her
-          hele dagen og etter hvert jobbe med å rigge ned etter festen.
+          komme til tunet for å spise rester og debriefe festen sammen.. De
+          nygifte vil være her og etter hvert jobbe med å rigge ned etter
+          festen.
         </p>
       </div>
       <div className="kartContainer">
@@ -36,7 +39,9 @@ export const Bryllupshelgen: React.FC = () => {
           <img src={kart} alt="kartbilde" />
         </div>
       </div>
-      <a href={kart} target="_blank" rel="noopener noreferrer"><i>Se kartet i stort format</i></a>
+      <a href={kart} target="_blank" rel="noopener noreferrer">
+        <i>Se kartet i stort format</i>
+      </a>
     </div>
   );
 };
